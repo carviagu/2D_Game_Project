1913d7fac0660b65bc2120276ab97b3e2ba9937e
Carlos Viñals <85288542+carviagu@users.noreply.github.com>
2025-01-01 22:29:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/2D_GAME_PROJECT", 
+    base: "/2D_Game_Project", 
     build: {
         minify: "terser",
     },
